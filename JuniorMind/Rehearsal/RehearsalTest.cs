1564934a9f1fb64172bb8c715da2e499d4459564
alinaoarga <oarga_alina@yahoo.com>
2015-12-01 00:23:08 +0200

    Calculate number of rehearsales for three rounds

--- JuniorMind/Rehearsal/RehearsalTest.cs
+++ JuniorMind/Rehearsal/RehearsalTest.cs
@@ -12,6 +12,14 @@ namespace Rehearsal
             int number = CalculateRehearsal(2, 2);
             Assert.AreEqual(4, number);
         }
+
+        [TestMethod]
+
+        public void thirdRehearsal()
+        {
+            int number = CalculateRehearsal(3, 3);
+            Assert.AreEqual(9, number);
+        }
         int CalculateRehearsal(int totalNumberOfRehearsales, int round)
         {
             return round * round;
