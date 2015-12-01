142ae0d35a09cda487d53ce3da9663ed71a43124
alinaoarga <oarga_alina@yahoo.com>
2015-12-01 19:55:14 +0200

    Calculating SecondMushroomsTest

--- JuniorMind/Mushrooms/MushroomsTest.cs
+++ JuniorMind/Mushrooms/MushroomsTest.cs
@@ -12,6 +12,12 @@ namespace Mushrooms
             int number = CalculateNumberOfRedMushrooms(15, 4);
             Assert.AreEqual(12, number);
         }
+        [TestMethod]
+        public void SecondMushroomsTest()
+        {
+            int number = CalculateNumberOfRedMushrooms(12, 2);
+            Assert.AreEqual(8, number);
+        }
         int CalculateNumberOfRedMushrooms(int totalNumberOfMushrooms, int differenceBetweenRedAndWhiteMushrooms)
         {
             int firstPartOfFormula = totalNumberOfMushrooms * differenceBetweenRedAndWhiteMushrooms;
