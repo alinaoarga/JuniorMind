647cb9aedc06e76cf41d3602f56253468dc51262
alinaoarga <oarga_alina@yahoo.com>
2015-12-03 12:18:16 +0200

    Calculate FirstBirdTest

--- JuniorMind/Trains/TrainsTest.cs
+++ JuniorMind/Trains/TrainsTest.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Trains
 {
     [TestClass]
-    public class UnitTest1
+    public class TrainsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstBirdTest()
         {
+            double distance = CalculateBirdDistance(70);
+            Assert.AreEqual(17.5d, distance);
+        }
+        double CalculateBirdDistance(double initialTrainDistance)
+        {
+            return initialTrainDistance / 4;
         }
     }
 }
