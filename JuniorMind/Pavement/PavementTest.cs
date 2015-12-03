b48b6df400fc91ec61bbb6e2df4de0908a7ed001
alinaoarga <oarga_alina@yahoo.com>
2015-12-03 14:04:01 +0200

    Calculate SecondCobblestoneTest

--- JuniorMind/Pavement/PavementTest.cs
+++ JuniorMind/Pavement/PavementTest.cs
@@ -12,7 +12,13 @@ namespace Pavement
            decimal number = CalculateCobblestoneNumber(6, 6, 4);
             Assert.AreEqual(4, number);
         }
-       decimal CalculateCobblestoneNumber(decimal squareWidth, decimal squareLenght, decimal cobblestoneSide)
+        [TestMethod]
+        public void SecondCobblestoneTest()
+        {
+            decimal number = CalculateCobblestoneNumber(3, 4, 2);
+            Assert.AreEqual(4, number);
+        }
+        decimal CalculateCobblestoneNumber(decimal squareWidth, decimal squareLenght, decimal cobblestoneSide)
         {
             decimal lenght = Math.Ceiling(squareLenght / cobblestoneSide);
             decimal width = Math.Ceiling(squareWidth / cobblestoneSide);
