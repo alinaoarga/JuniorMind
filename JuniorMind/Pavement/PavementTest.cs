c5f1e1e212b8acdfc4fcb07a78c27a89c7445ccf
alinaoarga <oarga_alina@yahoo.com>
2015-12-03 14:01:37 +0200

    Calculate FirstCobblestoneTest

--- JuniorMind/Pavement/PavementTest.cs
+++ JuniorMind/Pavement/PavementTest.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Pavement
 {
     [TestClass]
-    public class UnitTest1
+    public class PavementTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstCobblestoneTest()
         {
+           decimal number = CalculateCobblestoneNumber(6, 6, 4);
+            Assert.AreEqual(4, number);
+        }
+       decimal CalculateCobblestoneNumber(decimal squareWidth, decimal squareLenght, decimal cobblestoneSide)
+        {
+            decimal lenght = Math.Ceiling(squareLenght / cobblestoneSide);
+            decimal width = Math.Ceiling(squareWidth / cobblestoneSide);
+            return lenght * width;
         }
     }
 }
